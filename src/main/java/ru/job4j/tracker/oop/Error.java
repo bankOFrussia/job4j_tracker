73ec11bf50dfbf903750de467517ecbb5ee0f4a7
bankOFrussia <carlsbergrussia@gmail.com>
2022-10-09 19:30:37 +0300

    Конструктор

---
+++ src/main/java/ru/job4j/tracker/oop/Error.java
@@ -0,0 +1,30 @@
+package ru.job4j.tracker.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void getInfo() {
+        System.out.println("Действие: " + active);
+        System.out.println("Статус: " + status);
+        System.out.println("Сообщение: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        error.getInfo();
+        Error error101 = new Error(true, 101, "Произошёл сбой в серверной зоне.");
+        error101.getInfo();
+
+    }
+}
