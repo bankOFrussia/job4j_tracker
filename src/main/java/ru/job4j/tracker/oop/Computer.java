96febfc3012a7896e91cc56538bdb048d48c0989
bankOFrussia <carlsbergrussia@gmail.com>
2022-10-13 00:57:52 +0300

    Перегрузка конструктора

--- src/main/java/ru/job4j/tracker/oop/Computer.java
+++ src/main/java/ru/job4j/tracker/oop/Computer.java
@@ -11,6 +11,17 @@ public class Computer {
         this.cpu = cpu;
     }
 
+    public Computer(int ssd, String cpu) {
+        this.ssd = ssd;
+        this.cpu = cpu;
+    }
+
+    public Computer(boolean multiMonitor, double ssd, String cpu) {
+        this.multiMonitor = multiMonitor;
+        this.ssd = (int) ssd;
+        this.cpu = cpu;
+    }
+
     public void printInfo() {
         System.out.println("Много мониторов: " + multiMonitor);
         System.out.println("SSD: " + ssd + " Gb");
@@ -20,5 +31,11 @@ public class Computer {
     public static void main(String[] args) {
         Computer computer = new Computer(true, 256, "Intel Core I7-10700K");
         computer.printInfo();
+        Computer second = new Computer(true, 500, "Intel Core I7-10700K");
+        second.printInfo();
+        Computer third = new Computer(256, "AMD Ryzen 5 3600");
+        third.printInfo();
+        Computer forth = new Computer(true, 512.0, "AMD Ryzen 7 3700X");
+        forth.printInfo();
     }
 }
