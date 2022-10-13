52518e454d5ef008a4ae63a35d6660d7a17b37c6
bankOFrussia <carlsbergrussia@gmail.com>
2022-10-14 01:04:25 +0300

    Date. Отображение даты

---
+++ src/main/java/ru/job4j/tracker/oop/StartUI.java
@@ -0,0 +1,10 @@
+package ru.job4j.tracker.oop;
+
+import ru.job4j.tracker.Item;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item item = new Item();
+        System.out.println(item.getLocalDateTime());
+    }
+}
