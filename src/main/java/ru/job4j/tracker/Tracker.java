9525df3e6b0062d9ed295d73aca099dcbe270d69
bankOFrussia <carlsbergrussia@gmail.com>
2022-10-24 00:57:33 +0300

    Tracker - хранилище

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,31 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] array = new Item[size];
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            if (items[i] != null) {
+                array[i - count] = items[i];
+            } else {
+                count++;
+            }
+        }
+        return Arrays.copyOf(array, size - count);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] array = new Item[size];
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            if (items[i].getName().equals(key)) {
+                array[count] = items[i];
+                count++;
+            }
+        }
+        return Arrays.copyOf(array, count);
     }
 
     public Item findById(int id) {
