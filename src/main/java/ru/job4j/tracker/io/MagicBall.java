dc0dc09c64caffa0b3d2fd378bf544577e32754d
bankOFrussia <carlsbergrussia@gmail.com>
2022-10-28 23:52:08 +0300

    Чтение из консоли. Класс Scanner

--- src/main/java/ru/job4j/tracker/io/MagicBall.java
+++ src/main/java/ru/job4j/tracker/io/MagicBall.java
@@ -7,7 +7,7 @@ public class MagicBall {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Что ты хочешь узнать? ");
-        String question = sc.nextLine();
+        sc.nextLine();
         int answer = new Random().nextInt(3);
         if (answer == 0) {
             System.out.println("Да");
