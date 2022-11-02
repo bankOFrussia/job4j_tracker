2d5fcd90d544d96ba16d5242d360db93ce9dbc25
bankOFrussia <carlsbergrussia@gmail.com>
2022-11-02 21:07:27 +0300

    Интерфейс Input

--- src/main/java/ru/job4j/tracker/poly/Bus.java
+++ src/main/java/ru/job4j/tracker/poly/Bus.java
@@ -4,16 +4,17 @@ public class Bus implements Transport {
 
     @Override
     public void go() {
-
+        System.out.println("Поехали.");
     }
 
     @Override
     public void passengers(int number) {
-
+        System.out.println("Загрузили пассажиров.");
     }
 
     @Override
     public double getFuel(double fuelAmount) {
+        System.out.println("Заправили топливо.");
         return 0;
     }
 }
