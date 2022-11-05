e26bcb8f048e2b24dbed9de255ebedd5989fcd2f
bankOFrussia <carlsbergrussia@gmail.com>
2022-11-05 16:41:04 +0300

    Что такое исключение.

--- src/main/java/ru/job4j/tracker/ex/BackArray.java
+++ src/main/java/ru/job4j/tracker/ex/BackArray.java
@@ -1,4 +1,4 @@
-package ru.job4j.ex;
+package ru.job4j.tracker.ex;
 
 import java.util.Arrays;
 
@@ -8,8 +8,8 @@ public class BackArray {
         int middle = names.length / 2;
         for (int index = 0; index < middle; index++) {
             String temp = names[index];
-            names[index] = names[names.length - index];
-            names[names.length - index] = temp;
+            names[index] = names[names.length - index - 1];
+            names[names.length - index - 1] = temp;
         }
         System.out.println(Arrays.toString(names));
     }
